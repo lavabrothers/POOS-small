bbe9343bc14bc1a8ee2478d407f9b3b3230382d8
pau1h <hawkinsanthonypaul@gmail.com>
2025-01-18 16:57:54 -0500

    updated database login details

--- LAMPAPI/SearchColors.php
+++ LAMPAPI/SearchColors.php
@@ -5,7 +5,7 @@
 	$searchResults = "";
 	$searchCount = 0;
 
-	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	$conn = new mysqli("localhost", "messenger", "WeLoveCOP4331", "UserData");
 	if ($conn->connect_error) 
 	{
 		returnWithError( $conn->connect_error );
