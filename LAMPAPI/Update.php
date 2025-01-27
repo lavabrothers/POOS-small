a6cfe46cebac4ee9c54ce3ea6878750a89aa97fa
pau1h <hawkinsanthonypaul@gmail.com>
2025-01-26 20:04:10 -0500

    add update functionality

---
+++ LAMPAPI/Update.php
@@ -0,0 +1,61 @@
+
+<?php
+
+	header("Access-Control-Allow-Origin: *");
+	header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+	header("Access-Control-Allow-Headers: Content-Type, Authorization");
+	header("Access-Control-Allow-Credentials: true");
+
+	$inData = getRequestInfo(); //you need first, last, email, phone, and userid to update the contact. unchanged values are still processed
+    $firstName = $inData["firstName"];
+    $lastName = $inData["lastName"];
+    $email = $inData["email"];
+    $phoneNum = $inData["phoneNum"];
+    $userId = $inData["userId"];	
+
+	$conn = new mysqli("localhost", "messenger", "WeLoveCOP4331", "UserData"); 	
+	if( $conn->connect_error )
+	{
+		returnWithError( $conn->connect_error );
+	}
+	else
+	{
+		$stmt = $conn->prepare("INSERT INTO Contacts (First, Last, Phone, Email, UserID) VALUES (?,?,?,?,?) ON DUPLICATE KEY UPDATE First = VALUES(First), Last = VALUES(Last), Phone = VALUES(Phone), Email = VALUES(Email)");
+        $stmt->bind_param("ssssi", $firstName, $lastName, $phoneNum, $email,$userId);
+
+        if($stmt->execute()){
+            returnWithInfo($firstName, $lastName, $phoneNum, $userId, $email);
+        }else{
+            returnWithError("Unable to update contact");
+        }
+
+		$stmt->close();
+		$conn->close();
+	}
+
+
+	
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+        $retValue = '{"UserId":,"firstName":"","lastName":"","phone":"","email":"","error":"' . $err .'"}';
+		sendResultInfoAsJson( $retValue );
+	}
+    function returnWithInfo($first, $last, $phone, $email, $userid)
+    {
+        $retValue = '{"UserId":' . $userid . ',"firstName":"' . $first . '","lastName":"' . $last . '","phone":"' . $phone . '","email":"' . $email . '","error":""}';
+        sendResultInfoAsJson($retValue);
+    }
+
+	
+?>
