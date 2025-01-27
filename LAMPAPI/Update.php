56d65db0c87fe15c6586a57b4ea6e535d200b5dc
pau1h <hawkinsanthonypaul@gmail.com>
2025-01-26 20:17:18 -0500

    fixed update func

--- LAMPAPI/Update.php
+++ LAMPAPI/Update.php
@@ -6,12 +6,13 @@
 	header("Access-Control-Allow-Headers: Content-Type, Authorization");
 	header("Access-Control-Allow-Credentials: true");
 
-	$inData = getRequestInfo(); //you need first, last, email, phone, and userid to update the contact. unchanged values are still processed
+	$inData = getRequestInfo(); //you need first, last, email, phone, contactID, and userid to update the contact. unchanged values are still processed
     $firstName = $inData["firstName"];
     $lastName = $inData["lastName"];
     $email = $inData["email"];
     $phoneNum = $inData["phoneNum"];
     $userId = $inData["userId"];	
+    $contID = $inData["contID"];
 
 	$conn = new mysqli("localhost", "messenger", "WeLoveCOP4331", "UserData"); 	
 	if( $conn->connect_error )
@@ -20,11 +21,11 @@
 	}
 	else
 	{
-		$stmt = $conn->prepare("INSERT INTO Contacts (First, Last, Phone, Email, UserID) VALUES (?,?,?,?,?) ON DUPLICATE KEY UPDATE First = VALUES(First), Last = VALUES(Last), Phone = VALUES(Phone), Email = VALUES(Email)");
-        $stmt->bind_param("ssssi", $firstName, $lastName, $phoneNum, $email,$userId);
+		$stmt = $conn->prepare("INSERT INTO Contacts (CondID, First, Last, Phone, Email, UserID) VALUES (?,?,?,?,?,?) ON DUPLICATE KEY UPDATE ContID = VALUES(ContID), First = VALUES(First), Last = VALUES(Last), Phone = VALUES(Phone), Email = VALUES(Email), UserID = VALUES(UserID)");
+        $stmt->bind_param("issssi", $contID, $firstName, $lastName, $phoneNum, $email,$userId);
 
         if($stmt->execute()){
-            returnWithInfo($firstName, $lastName, $phoneNum, $userId, $email);
+            returnWithInfo($contID, $firstName, $lastName, $phoneNum, $userId, $email);
         }else{
             returnWithError("Unable to update contact");
         }
@@ -48,12 +49,12 @@
 	
 	function returnWithError( $err )
 	{
-        $retValue = '{"UserId":,"firstName":"","lastName":"","phone":"","email":"","error":"' . $err .'"}';
+        $retValue = '{"ContID":,"UserId":,"firstName":"","lastName":"","phone":"","email":"","error":"' . $err .'"}';
 		sendResultInfoAsJson( $retValue );
 	}
-    function returnWithInfo($first, $last, $phone, $email, $userid)
+    function returnWithInfo($contID, $first, $last, $phone, $email, $userid)
     {
-        $retValue = '{"UserId":' . $userid . ',"firstName":"' . $first . '","lastName":"' . $last . '","phone":"' . $phone . '","email":"' . $email . '","error":""}';
+        $retValue = '{"ContID":'. $contID .',"UserId":' . $userid . ',"firstName":"' . $first . '","lastName":"' . $last . '","phone":"' . $phone . '","email":"' . $email . '","error":""}';
         sendResultInfoAsJson($retValue);
     }
 
