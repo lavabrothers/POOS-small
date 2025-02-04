d95378bf737746cbbd30e6a244d9a8327acc61ef
Evan Rees <evan@thereesgroup.com>
2025-02-04 13:41:16 -0500

    fixed logout function

--- home.js
+++ home.js
@@ -196,6 +196,16 @@ function showCreateContactForm() {
     document.querySelector('.createContactForm').style.display = 'block';
 }
 
+function doLogout() {
+    userId = 0;
+    firstName = "";
+    lastName = "";
+    document.cookie = "firstName= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
+    document.cookie = "lastName= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
+    document.cookie = "userId= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
+    window.location.href = "index.html"; // go to the login page
+}
+
 document.getElementById('createContactForm').addEventListener('submit', function(event) {
     event.preventDefault();
     const userId = readCookie(); // Get the user ID from the cookie
